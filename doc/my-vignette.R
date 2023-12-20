9a8c29c20f99b29d6aac0bf530a2e0a23ee26016
lawrencejesudasan <130832132+lawjesudasan@users.noreply.github.com>
2023-12-20 19:22:59 +0100

    Modifiy the repo structure

---
+++ doc/my-vignette.R
@@ -0,0 +1,17 @@
+## ---- include = FALSE---------------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+
+## ----setup--------------------------------------------------------------------
+library(immoswiss)
+
+## ----eval=FALSE---------------------------------------------------------------
+#  install.packages("immoswiss")
+
+## ----eval=FALSE---------------------------------------------------------------
+#  # Example usage of estimate_price()
+#  result <- estimate_price(2, 100, 1004, lausanne)
+#  print(result)
+
